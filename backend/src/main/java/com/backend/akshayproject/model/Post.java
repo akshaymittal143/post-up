d481b861b8a409c0f6aec31fb79859bff8be8526
Akshay Mittal <akshay.mittal@slalom.com>
2018-09-04 12:00:40 -0500

    create Post entity using lombok and mongo annotations

---
+++ backend/src/main/java/com/backend/akshayproject/model/Post.java
@@ -0,0 +1,30 @@
+package com.backend.akshayproject.model;
+
+import java.io.Serializable;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post implements Serializable{
+
+  @Id
+  private String id;
+
+  @NotBlank
+  private String title;
+
+  @NotBlank
+  private String content;
+
+}
