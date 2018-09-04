240514190daed382f78cbe4abc8466cea355fe48
Akshay Mittal <akshay.mittal@slalom.com>
2018-09-04 14:11:20 -0500

    optimize imports

--- backend/src/main/java/com/backend/akshayproject/AkshayProjectApplication.java
+++ backend/src/main/java/com/backend/akshayproject/AkshayProjectApplication.java
@@ -1,21 +1,7 @@
 package com.backend.akshayproject;
 
-import com.backend.akshayproject.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authorization.AuthorizationDecision;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authorization.AuthorizationContext;
-import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
-import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class AkshayProjectApplication {
