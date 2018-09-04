af77389b65bc7a8c5952ddcdd0ba71afd33f75b3
Akshay Mittal <akshay.mittal@slalom.com>
2018-09-04 10:45:09 -0500

    backend setup

---
+++ backend/src/main/java/com/backend/akshayproject/AkshayProjectApplication.java
@@ -0,0 +1,12 @@
+package com.backend.akshayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkshayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkshayProjectApplication.class, args);
+	}
+}
