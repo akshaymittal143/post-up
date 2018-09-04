b58d154925ed39ee9c2b1bf6dda428b0cfc841ac
Akshay Mittal <akshay.mittal@slalom.com>
2018-09-04 17:52:48 -0500

    lombok setup

--- backend/src/main/java/com/backend/akshayproject/model/Post.java
+++ backend/src/main/java/com/backend/akshayproject/model/Post.java
@@ -4,40 +4,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  @NotNull
   private String title;
 
+  @NotNull
   private String content;
 
 }
