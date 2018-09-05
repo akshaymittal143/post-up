13905535e7ac6ff5c51b3cbb4c35000f48d5b3e0
Akshay Mittal <akshay.mittal@slalom.com>
2018-09-05 13:29:08 -0500

    prettier-integration

--- client/src/app/post/post.service.ts
+++ client/src/app/post/post.service.ts
@@ -1,32 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Post } from '../model/post';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Post } from "../model/post";
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   })
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PostService {
+  baseUrl = "http://localhost:8080/api/posts";
 
-  baseUrl = 'http://localhost:8080/api/posts';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getPosts(){
+  getPosts() {
     return this.http.get(this.baseUrl);
   }
 
   getPost(id: number) {
-    return this.http.get(this.baseUrl + '/' + id);
+    return this.http.get(this.baseUrl + "/" + id);
   }
 
-  createPost(post){
+  createPost(post) {
     const body = JSON.stringify(post);
     return this.http.post(this.baseUrl, body, httpOptions);
   }
