4d1b894cc1c433170af00a117f22d020b63512ea
Akshay Mittal <akshay.mittal@slalom.com>
2018-09-18 13:35:14 -0500

    backend: using in-memory db:h2 & modified Post model with @table and added createDate property

--- backend/src/main/java/com/backend/akshayproject/model/Post.java
+++ backend/src/main/java/com/backend/akshayproject/model/Post.java
@@ -1,30 +1,45 @@
 package com.backend.akshayproject.model;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "post")
 public class Post {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "post_id")
   private long id;
 
-  @NotNull
-  @Column(name = "title")
+  @Column(name = "title", nullable = false)
+  @Length(min = 5, message = "*Your title must have at least 5 characters")
   private String title;
 
   @NotNull
-  @Column(name = "content")
+  @Column(name = "content", columnDefinition = "TEXT")
   private String content;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "create_date", nullable = false, updatable = false)
+  @CreationTimestamp
+  private Date createDate;
+
 }
